b75c18f125f722068fe9acc5190efe0045e76e74
xuetaoliu-cls <xuetao.liu@lightsource.ca>
2015-07-02 16:07:31 -0600

    Remove the warning in main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
 
-	AMDSCentralServer *centralServer = new AMDSCentralServer();
+	new AMDSCentralServer();
 
 	return a.exec();
 }
