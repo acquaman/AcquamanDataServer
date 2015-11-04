65c714fc3337ccacfb2a190e80719846149bc47b
Xuetao (Sheldon) Liu <xuetao.s.liu@gmail.com>
2015-11-04 10:22:20 -0600

    Remove unused function `at` for AMDSBufferGroup

--- source/DataElement/AMDSBufferGroup.h
+++ source/DataElement/AMDSBufferGroup.h
@@ -35,8 +35,6 @@ public:
 	/// returns the current dataholder of cumulative data
 	AMDSDataHolder *cumulativeDataHolder() const;
 
-	/// returns the dataHolder at given index
-	AMDSDataHolder* at(int index);
 	/// The number of items currently stored in the buffer (once the buffer reaches maxSize, this will always return maxSize)
 	int count() const;
 	/// Clears the buffer of all its members, and frees their resources
