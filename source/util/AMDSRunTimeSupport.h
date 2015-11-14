42051156c6a3bad92c6bde3ff1db4da87d9ac85a
Darren Hunter <darren.hunter@usask.ca>
2015-11-14 13:49:24 -0800

    Update AMDSRunTimeSupport.h
    Adding const to the function name.

--- source/util/AMDSRunTimeSupport.h
+++ source/util/AMDSRunTimeSupport.h
@@ -8,13 +8,13 @@ namespace AMDSRunTimeSupport {
 	extern quint8 debugLevel_;
 
 	/// Returns the current debug level
-	quint8 debugLevel();
+	quint8 debugLevel() const;
 
 	/// Sets the debug level
 	void setDebugLevel(quint8 debugLevel);
 
 	/// Returns true if the requested level is less than or equal to the current level. So, you can pass in the relative level of your information (higher number is a higher level of debugging information) and get a bool as to whether you should print or not
-	bool debugAtLevel(quint8 requestedLevel);
+	bool debugAtLevel(quint8 requestedLevel) const;
 }
 
 #endif // AMDSRUNTIMESUPPORT_H
